0786cbef9cabfe1817bfa7131916345e97e26b25
Danylo <danilovityk@gmail.com>
2023-11-15 13:45:32 -0500

    isOrderFilled left to implement

---
+++ MS2/CustomerOrder.cpp
@@ -0,0 +1,8 @@
+//
+//  CustomerOrder.cpp
+//  milestone 2
+//
+//  Created by Danik on 09.11.2023.
+//
+
+#include "CustomerOrder.hpp"
---
+++ MS2/CustomerOrder.h
@@ -0,0 +1,13 @@
+//
+//  CustomerOrder.hpp
+//  milestone 2
+//
+//  Created by Danik on 09.11.2023.
+//
+
+#ifndef CustomerOrder_hpp
+#define CustomerOrder_hpp
+
+#include <stdio.h>
+
+#endif /* CustomerOrder_hpp */
